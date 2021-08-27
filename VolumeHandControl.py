e76425d74fcfa917bf5f951fe9916909f75c900a
victor <victor.lvl47@gmail.com>
2021-08-26 23:58:34 -0600

    volume control with hand gestures

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
+import alsaaudio
 
 wCam, hCam = 640, 480
 
@@ -13,6 +14,22 @@ cap.set(4, hCam)
 pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
+
+# Mixer object, provides access to the ALSA mixer API.
+mixer = alsaaudio.Mixer()
+channel = alsaaudio.MIXER_CHANNEL_ALL
+
+# max volume
+maxVol = 100
+# min volume
+minVol = 0
+# minimum length between the thumb and the index finger 
+# to reach the minVol.
+minLength = 50
+# maximum length between the thumb and the index finger 
+# to reach the maxVol.
+maxLength = 200
+
 
 while True:
     success, img = cap.read()
@@ -41,9 +58,30 @@ while True:
         length = math.hypot(x2 - x1, y2 - y1)
         print(length)
 
-        if length<50:
-            cv2.circle(img, (cx, cy), 15, (0, 255, 0), 
+        vol = np.interp(length, [minLength, maxLength], 
+            [minVol, maxVol])
+        mixer.setvolume(int(vol), channel)
+    
+        # current volume text
+        cv2.putText(img, str(int(vol)), (cx - 20, cy + 10), 
+            cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+        if length<minLength:
+            cv2.circle(img, (cx, cy), 15, (77, 77, 77), 
                 cv2.FILLED)
+            # this is a hack to draw the 
+            # text in fron of the circle
+            cv2.putText(img, str(int(vol)), (cx - 20, cy + 10), 
+                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        if length>maxLength:
+            # rgb(57, 255, 20)
+            cv2.circle(img, (cx, cy), 15, (20, 255, 57), 
+                cv2.FILLED)
+            # this is a hack to draw the 
+            # text in fron of the circle
+            cv2.putText(img, str(int(vol)), (cx - 20, cy + 10), 
+                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
 
     # Frame rate
     cTime = time.time()
