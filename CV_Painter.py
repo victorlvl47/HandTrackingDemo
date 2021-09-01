2a3b953d5a1c7770990e29445807d95340ce3248
victor <victor.lvl47@gmail.com>
2021-08-31 20:37:28 -0600

    overlay the color selection banner

--- CV_Painter.py
+++ CV_Painter.py
@@ -6,10 +6,24 @@ import HandTrackingModule as htm
 
 folderPath = "paintMenuImages"
 myList = os.listdir(folderPath)
+myList.sort()
 print(myList)
 overlayList = []
 for imPath in myList:
     image = cv2.imread(f'{folderPath}/{imPath}')
     overlayList.append(image)
-
 print(len(overlayList))
+header = overlayList[0]
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 800)
+cap.set(4, 600)
+
+while True:
+    success, img = cap.read()
+
+    # overlay the selection menu
+    img[0:122, 0:800] = header
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
