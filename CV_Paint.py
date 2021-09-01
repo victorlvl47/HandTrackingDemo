80c845295cb40c037bd699b8dd21aeb4878ee667
victor <victor.lvl47@gmail.com>
2021-09-01 17:59:12 -0600

    Updated the readme

--- CV_Paint.py
+++ CV_Paint.py
@@ -4,6 +4,9 @@ import time
 import os
 import HandTrackingModule as htm
 
+brushThickness = 15
+eraserThickness = 50
+
 folderPath = "paintMenuImages"
 myList = os.listdir(folderPath)
 myList.sort()
@@ -24,6 +27,12 @@ cap.set(4, 600)
 
 detector = htm.handDetector(detectionCon=0.85)
 
+# previous x/y-coordinate
+xp, yp = 0, 0
+
+# canvas where we will be drawing
+imgCanvas = np.zeros((600, 800, 3), np.uint8)
+
 while True:
     success, img = cap.read()
     # flip the video, for more intuitive drawing
@@ -43,6 +52,7 @@ while True:
 
         # two fingers are up, selection mode
         if fingers[1] and fingers[2]:
+            xp, yp = 0, 0
             print("selection mode")
             # color selection or eraser
             if y1 < 122:
@@ -63,8 +73,8 @@ while True:
                 # select eraser
                 elif 590 < x1 < 800:
                     header = overlayList[3]
-                    #
-                    drawColor = (255, 255, 255)
+                    # white
+                    drawColor = (0, 0, 0)
 
             cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
             print("selection mode")
@@ -72,12 +82,35 @@ while True:
         # index finger is up, drawing mode
         if fingers[1] and fingers[2] == False:
             cv2.circle(img, (x1, y1), 15, drawColor, cv2.FILLED)
+
+            # for the first frame
+            if xp == 0 and yp == 0:
+                xp, yp = x1, y1
+
+            # for the eraser, we make the brush bigger
+            if drawColor == (0, 0, 0):
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, eraserThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
+            # we just draw normally
+            else:
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+
+            xp, yp = x1, y1
             print('drawing mode')
 
+    imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img, imgInv)
+    img = cv2.bitwise_or(img, imgCanvas)
 
 
     # overlay the color selection
     img[0:122, 0:800] = header
 
+    # img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0)
+
     cv2.imshow("Image", img)
+    # cv2.imshow("Canvas", imgCanvas)
     cv2.waitKey(1)
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Hand Tracking Demo
 
+Note: Click the images to go to the video.
+
 #### Hand Tracking
 
 [![Hand Tracking Thumbnail](https://img.youtube.com/vi/LAQ-O3kCK8c/0.jpg)](https://www.youtube.com/watch?v=LAQ-O3kCK8c)
@@ -14,6 +16,8 @@ python MyNewGameHandTraking.py
 ```
 
 #### Hand Gesture as a Control
+
+[![Hand Gesture Thumbnail](https://img.youtube.com/vi/P3x2iqfM-tU/0.jpg)](https://www.youtube.com/watch?v=P3x2iqfM-tU)
 
 Use hand gestures to control the volume of your
 computer in this case, this demo could be
@@ -34,3 +38,16 @@ python VolumeHandControl.py
 tested on ubuntu, porting this demo to windows
 should be really straightforward instead of using
 [pyalsaaudio](https://github.com/larsimmisch/pyalsaaudio) use [pycaw](https://github.com/AndreMiras/pycaw).
+
+#### CV Paint Demo
+
+A MS Paint like application but you use your hand
+as the cursor to paint and select colors and tools.
+
+###### Setup
+* Activate your [virtual environment](https://docs.python.org/3/library/venv.html) for python3.
+
+``` bash
+# Run the demo, type in command line
+python CV_Paint.py
+```
